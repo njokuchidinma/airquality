306b97cd342c56d98b180e9bc620ff19c378afcf
njokuchidinma <njokuchidinma16@gmail.com>
2024-10-17 15:07:10 +0100

    version 1 updated for pythonanywhere(2)

--- AQI/urls.py
+++ AQI/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path, include
 
 
@@ -23,3 +25,5 @@ urlpatterns = [
     # path('api-auth/', include('rest_framework.urls')),
     path('api/v1/', include('API.urls') ),
 ]
+
+urlpatterns+=static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
